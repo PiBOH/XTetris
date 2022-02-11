37b895bcb47f342cb5d122997e27a85ea654a7d1
Alex Giulio Berton <alexgiulioberton@icloud.com>
2022-02-11 16:40:50 +0100

    Minor commit

--- main.c
+++ main.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "Tetramino/tetramino.h"
 
 int main() {
@@ -8,38 +7,5 @@ int main() {
     t.rotazione = ADD180;
 
     print_tetramino(t);
-/*
-    printf("basic:\n");
-    for (i = 0; i < DIM; ++i) {
-        for (j = 0; j < DIM; ++j) {
-            printf("%d  ", t.stato_BASIC[i][j]);
-        }
-        printf("\n");
-    }
-
-    printf("add90:\n");
-    for (i = 0; i < DIM; ++i) {
-        for (j = 0; j < DIM; ++j) {
-            printf("%d  ", t.stato_ADD90[i][j]);
-        }
-        printf("\n");
-    }
-
-    printf("add180:\n");
-    for (i = 0; i < DIM; ++i) {
-        for (j = 0; j < DIM; ++j) {
-            printf("%d  ", t.stato_ADD180[i][j]);
-        }
-        printf("\n");
-    }
-
-    printf("add270:\n");
-    for (i = 0; i < DIM; ++i) {
-        for (j = 0; j < DIM; ++j) {
-            printf("%d  ", t.stato_ADD270[i][j]);
-        }
-        printf("\n");
-    }
-*/
     return 0;
 }
