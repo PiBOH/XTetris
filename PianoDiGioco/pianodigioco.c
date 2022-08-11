49114494df359c943bdfbef04fc94245a3c1afb3
Alex Giulio Berton <alexgiulioberton@icloud.com>
2022-08-11 13:09:23 +0200

    [PIANODIGIOCO] - Aggiunta documentazione ai metodi

--- PianoDiGioco/pianodigioco.c
+++ PianoDiGioco/pianodigioco.c
@@ -58,62 +58,108 @@ void print_pianodigioco_basic(const PianoDiGioco_t p)
     printf("- - - - - - - - - -\n0 1 2 3 4 5 6 7 8 9\n");
 }
 
-/* TODO: Documentazione */
-Bool_t __check_rigavuota__(PianoDiGioco_t p, int matricetetrmaino[4][4], int row, int col, int amp, int row_tetramino)
+/**
+ * Il metodo ha il compito di verificare, scorrendo la matrice del piano di gioco fornita come parametro se
+ * nell'intervallo definito dalla colonna di partenza (ovvero quella data dalla sottrazione tra la colonna selezionata
+ * e l'ampiezza del tetramino scelto) e quella di fine (che quindi diventa la colonna scelta) esistono dei punti di
+ * collisione tra un pezzo già settato nel piano di gioco e quello che si vuole calare. Se ciò accade significa che non
+ * è possibile calare il tetramino in questa riga.
+ * @param p il piano di gioco, contenente quindi la matrice di gioco
+ * @param matrice_tetramino matrice di interi che rappresenta un tetramino (quello che si vuole posizionare)
+ * @param row la riga in questione da verificare come libera
+ * @param col la colonna di partenza (scelta sulla base di un posizionamento a partire dal lato in basso a dx
+ * della matrice del tetramino
+ * @param amp l'ampiezza orizzontale del tetramino selezionato
+ * @param row_tetramino la riga della matrice_tetramino da verificare simultaneamente alla riga della <code>matrice_di_gioco</code>
+ * @return <code>TRUE</code> - se la riga è valida per un inserimento\n
+ *         <code>FALSE</code> - se nella riga non è possibile posizionare nulla
+ */
+Bool_t __check_rigavuota__(PianoDiGioco_t p, int matrice_tetramino[4][4], int row, int col, int amp, int row_tetramino)
 {
     int j, k = 4 - amp;
 
     for (j = col - amp + 1; j <= col; ++j, ++k)
-        if (!p.matrice_di_gioco[row][j].is_vuota && matricetetrmaino[row_tetramino][k]) // cerca collisioni
+        if (!p.matrice_di_gioco[row][j].is_vuota && matrice_tetramino[row_tetramino][k]) // cerca collisioni
             return FALSE;
 
     return TRUE;
 }
 
-/* TODO: Implementare */
-/* TODO: Documentazione */
+/**
+ * Metodo che ha il compito di posizionare il tetramino scelto dall'utente sul piano di gioco in base alla colonna da
+ * lui scelta. Viene poi anche verificato il numero di righe che sono state rimosse con l'aggiunta del tetramino stesso
+ * e in base ad esse viene attribuito un punteggio (vedi poi) all'utente passato come parametro del metodo.\n
+ * \n
+ * 1 riga -> 1 punto\n
+ * 2 righe -> 3 punti\n
+ * 3 righe -> 6 punti\n
+ * 4 righe -> 12 punti\n
+ * @param p il piano di gioco contenente la matrice di gioco su cui posizionare il tetramino
+ * @param player il giocatore che ha effettuato la mossa in analisi
+ * @param t il tetramino da posizionare
+ * @param col la colonna di partenza della matrice di gioco (sulla quale basarsi per il posizionamento)
+ * @return <code>TRUE</code> - se il tetramino è stato correttamente posizionato\n
+ *         <code>FALSE</code> - in caso di qualsiasi errore (tetramino non posizionabile nella colonna scelta)
+ */
 Bool_t set_tetraminosupianodigioco(PianoDiGioco_t* p, Player_t* player, Tetramino_t t, int col)
 {
     int i, j;
-    Bool_t empty_r = FALSE;
+    Bool_t is_posizionabile = FALSE;
     int row;
 
-    for (j = ROWS - 1; j >= 0 && empty_r == FALSE; --j) {
-        int row_t = 3; // riga tetramino
-        empty_r = TRUE;
+    /* Prima di tutto verifico se il pezzo è posizionabile orizzontalmente */
+    if ((col + 1) - t.ampiezze[t.rotazione] < 0)
+        return FALSE;
+
+    /* Scorrimento di tutte le righe dall'ultima alla prima per cercare di posizionare il tetramino */
+    for (j = ROWS - 1; j >= 0; --j) {
+        int row_t = 3; /* parto sempre dall'ultima riga del tetramino a verificare */
+        is_posizionabile = TRUE;
         row = j;
 
-        for (i = j; i > j - get_altezzatetramino(t) && empty_r; --i) {
+        /* ciclo che mi permette di verificare se il tetramino è piazzabile a partire dalla riga dettata dal for esterno */
+        for (i = j; i > j - get_altezzatetramino(t) && is_posizionabile; --i) {
             switch (t.rotazione) {
                 case BASIC:
-                    empty_r = __check_rigavuota__(*p, t.stato_BASIC, i, col, t.ampiezze[t.rotazione], row_t--) && empty_r;
+                    is_posizionabile = __check_rigavuota__(*p, t.stato_BASIC, i, col, t.ampiezze[t.rotazione], row_t--);
                     break;
                 case ADD90:
-                    empty_r = __check_rigavuota__(*p, t.stato_ADD90, i, col, t.ampiezze[t.rotazione], row_t--) && empty_r;
+                    is_posizionabile = __check_rigavuota__(*p, t.stato_ADD90, i, col, t.ampiezze[t.rotazione], row_t--);
                     break;
                 case ADD180:
-                    empty_r = __check_rigavuota__(*p, t.stato_ADD180, i, col, t.ampiezze[t.rotazione], row_t--) && empty_r;
+                    is_posizionabile = __check_rigavuota__(*p, t.stato_ADD180, i, col, t.ampiezze[t.rotazione], row_t--);
                     break;
                 default:
-                    empty_r = __check_rigavuota__(*p, t.stato_ADD270, i, col, t.ampiezze[t.rotazione], row_t--) && empty_r;
+                    is_posizionabile = __check_rigavuota__(*p, t.stato_ADD270, i, col, t.ampiezze[t.rotazione], row_t--);
                     break;
             }
         }
 
+        /* Mi ri-posiziono nella riga da cui sono partito (quella dettata dal for esterno) */
         i = row;
 
-        if ((col + 1) - t.ampiezze[t.rotazione] < 0) {
-            return FALSE;  // sfondo in ampiezza
-        }
-
-
-
-        if (empty_r) {
+        /*
+         * Se la variabile is_posizionabile è settata a true vuol dire che ho controllato tutte le righe e colonne
+         * interessate e non ho trovato collisioni, quindi posso diseganare sulla matrice di gioco
+         */
+        if (is_posizionabile) {
             int k, q, i1, j1;
+
+            /*
+             * Eseguo un'ulteriore verifica per vedere se il posizionamento mi vada a sforare verticalemente il piano
+             * di gioco. In questo caso l'utente ha perso perchè ha raggiunto il limite superiore della matrice
+             */
             if ((i - get_altezzatetramino(t) + 1) < 0) {
                 p->is_limiteraggiunto = TRUE;
                 return FALSE;
             }
+
+            /*
+             * Cella per cella verifico se essa è settata ad 1 nella matrice del tetramino scelto e di pari passo vado
+             * a settare come piene le celle del piano di gioco, salvando anche il tetramino che ci vado a posizionare
+             * per poi in fase di stampa andare a colorare correttamente la cella in base al tetramino contenuto in
+             * essa
+             */
             for (k = i, i1 = 3; k > i - get_altezzatetramino(t); --k, i1--) {
                 for (q = col, j1 = 3; q > col - t.ampiezze[t.rotazione]; --q, j1--) {
                     if (t.stato_BASIC[i1][j1] && t.rotazione == BASIC ||
